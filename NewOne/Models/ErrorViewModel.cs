bb196e1fe13ba9bc84cd3dcadc47fd3a2977ba39
Sraya Avraham <sraya305@gmail.com>
2019-04-01 21:27:10 +0300

    Add project files.

---
+++ NewOne/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewOne.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
